49b03a1c47fc581ac25e8683b439f21c5ec37975
jamshee <jamshee@gmail.com>
2020-10-31 14:58:04 -0400

    Initial commit

---
+++ src/main/java/com/jam/springboot/JamSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jam.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamSpringBootSampleApplication.class, args);
+	}
+
+}
